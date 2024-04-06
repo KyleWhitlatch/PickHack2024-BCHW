3eee7ebff949528dcb54e93d129ea79828cc939e
kaiserl4961 <ltklionel@gmail.com>
2024-04-06 17:38:29 -0500

    Update database.py

--- py_src/database.py
+++ py_src/database.py
@@ -8,16 +8,18 @@ class testmongo:
         self.users = self.db['users']
         self.posts = self.db.posts
         
+    # Store the username and smart contract address in the database
     def store_userpass(self, username, address):
         post = {"username": username, "sc_address": address}
         id = self.users.insert_one(post).inserted_id
         print(id)
         
+    # Retrieve the username and smart contract address from the database
     def retrieve_userpass(self, username):
         user = self.users.find_one({'username': username})
         return user
         
-        
+# Test the database functions
 test = testmongo()
 test.store_userpass('test_user', 'test_address')
 query = test.retrieve_userpass('test_user')
