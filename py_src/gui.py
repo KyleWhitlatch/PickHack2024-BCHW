5aa87c088b42d98c1cf26fd5ea949714ee71295e
GharretWilcut <gharretwilcut@gmail.com>
2024-04-06 15:08:04 -0500

    Update gui.py

--- py_src/gui.py
+++ py_src/gui.py
@@ -1,8 +1,10 @@
 import customtkinter as ctk 
 import tkinter.messagebox as tkmb 
 from validate_email import validate_email
+import class_info
   
-  
+
+member = class_info.info()
 # Selecting GUI theme - dark, light , system (for system default) 
 ctk.set_appearance_mode("dark") 
   
@@ -25,6 +27,8 @@ def signup():
     new_window.geometry("350x150") 
     if is_valid and user_pass.get() == user_pass_rep.get(): 
         ctk.CTkLabel(new_window,text="YOU JUST LOST THE GAME!!").pack() 
+        member.set_pw_hash(user_pass.get)
+        member.hash_string_gen()
     elif is_valid and user_pass.get() != user_pass_rep.get(): 
         tkmb.showwarning(title='Wrong password',message='Please check your passwords match') 
     elif not is_valid and user_pass.get() == user_pass_rep.get(): 
@@ -44,8 +48,8 @@ def login():
     is_valid = validate_email(user_entry.get())
   
     if user_entry.get() == username and user_pass.get() == password: 
-        
         ctk.CTkLabel(new_window,text="YOU JUST LOST THE GAME!!").pack() 
+        member.hash_with_keccak()
     elif is_valid and user_pass.get() != password and username == user_entry.get(): 
         tkmb.showwarning(title='Wrong password',message='Please check your password') 
     elif (not is_valid) and user_pass.get() == password: 
