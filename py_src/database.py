045c939593ab6af4df9848bedec0852f73f974d0
kaiserl4961 <ltklionel@gmail.com>
2024-04-06 17:35:13 -0500

    Update database.py

--- py_src/database.py
+++ py_src/database.py
@@ -1,40 +1,24 @@
 from pymongo import MongoClient
+import json
 
-# Create a client to connect to MongoDB
-client = MongoClient('localhost', 27017)
-
-# Create a new database
-db = client['login_db']
-
-# Create a collection named 'users'
-users = db['users']
-
-# Function to store a new username
-def store_username(username):
-    # Check if the username already exists
-    if users.find_one({'username': username}):
-        return "Username already exists"
-    else:
-        # Insert the new username
-        users.insert_one({'username': username})
-        return "Username stored successfully"
-
-def store_hashed_password(password):
-    # Check if the password already exists
-    if users.find_one({'password': password}):
-        return "Password already exists"
-    else:
-        # Insert the new password
-        users.insert_one({'password': password})
-        return "Password stored successfully"
-
-def store_hash(hash):
-    # Check if the hash already exists
-    if users.find_one({'hash': hash}):
-        return "Hash already exists"
-    else:
-        # Insert the new hash
-        users.insert_one({'hash': hash})
-        return "Hash stored successfully"
-    
-# print(store_username('test_user'))
+class testmongo:
+    def __init__(self):
+        self.client = MongoClient('localhost', 27017)
+        self.db = self.client['login_db']
+        self.users = self.db['users']
+        self.posts = self.db.posts
+        
+    def store_userpass(self, username, address):
+        post = {"username": username, "sc_address": address}
+        id = self.users.insert_one(post).inserted_id
+        print(id)
+        
+    def retrieve_userpass(self, username):
+        user = self.users.find_one({'username': username})
+        return user
+        
+        
+test = testmongo()
+test.store_userpass('test_user', 'test_address')
+query = test.retrieve_userpass('test_user')
+print(query['sc_address'])
